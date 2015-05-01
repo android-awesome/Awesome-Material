8cd06d7ed4eb911077d1b45c73259a159943e96d
Danielle Vass <danielle@de-velopment.com>
2015-05-01 21:46:33 +0100

    fix sample imports

--- AwesomeMaterialSample/app/src/main/java/cyd/awesome/material/sample/MainActivity.java
+++ AwesomeMaterialSample/app/src/main/java/cyd/awesome/material/sample/MainActivity.java
@@ -2,14 +2,8 @@ package cyd.awesome.material.sample;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 
-import material.awesome.android.cyd.com.awesomematerial.FontCharacterMaps;
-import material.awesome.android.cyd.com.awesomematerial.FontUtil;
-import sample.material.awesome.android.cyd.com.awesomematerialsample.R;
+import cyd.awesome.material.sample.R;
 
 public class MainActivity extends ActionBarActivity {
 
