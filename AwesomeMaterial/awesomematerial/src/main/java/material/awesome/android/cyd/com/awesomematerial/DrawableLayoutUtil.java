755f99e08eaabde4639df73a59ece10d2280c979
Danielle Vass <danielle@de-velopment.com>
2015-05-01 21:01:47 +0100

    add material design layout options

--- AwesomeMaterial/awesomematerial/src/main/java/material/awesome/android/cyd/com/awesomematerial/DrawableLayoutUtil.java
+++ AwesomeMaterial/awesomematerial/src/main/java/material/awesome/android/cyd/com/awesomematerial/DrawableLayoutUtil.java
@@ -23,6 +23,8 @@ public class DrawableLayoutUtil {
 
         possibleLayouts = new ArrayList<>();
 
+
+        //bootstrap layouts
         possibleLayouts.add(new DrawableLayout(0, R.drawable.button_bootstrap_square_default,
                 R.drawable.button_bootstrap_rounded_default, R.color.md_black_1000));
         possibleLayouts.add(new DrawableLayout(1, R.drawable.button_bootstrap_square_primary,
@@ -38,6 +40,45 @@ public class DrawableLayoutUtil {
         possibleLayouts.add(new DrawableLayout(6, R.drawable.button_bootstrap_square_inverse,
                 R.drawable.button_bootstrap_rounded_inverse, R.color.md_white_1000_transparent90));
 
+        //material design layouts
+        possibleLayouts.add(new DrawableLayout(7, R.drawable.button_material_square_red,
+                R.drawable.button_material_rounded_red, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(8, R.drawable.button_material_square_pink,
+                R.drawable.button_material_rounded_pink, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(9, R.drawable.button_material_square_purple,
+                R.drawable.button_material_rounded_purple, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(10, R.drawable.button_material_square_deep_purple,
+                R.drawable.button_material_rounded_deep_purple, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(11, R.drawable.button_material_square_indigo,
+                R.drawable.button_material_rounded_indigo, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(12, R.drawable.button_material_square_blue,
+                R.drawable.button_material_rounded_blue, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(13, R.drawable.button_material_square_light_blue,
+                R.drawable.button_material_rounded_light_blue, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(14, R.drawable.button_material_square_cyan,
+                R.drawable.button_material_rounded_cyan, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(15, R.drawable.button_material_square_teal,
+                R.drawable.button_material_rounded_teal, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(16, R.drawable.button_material_square_green,
+                R.drawable.button_material_rounded_green, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(17, R.drawable.button_material_square_light_green,
+                R.drawable.button_material_rounded_light_green, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(18, R.drawable.button_material_square_lime,
+                R.drawable.button_material_rounded_lime, R.color.md_black_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(19, R.drawable.button_material_square_yellow,
+                R.drawable.button_material_rounded_yellow, R.color.md_black_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(20, R.drawable.button_material_square_amber,
+                R.drawable.button_material_rounded_amber, R.color.md_black_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(21, R.drawable.button_material_square_orange,
+                R.drawable.button_material_rounded_orange, R.color.md_black_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(22, R.drawable.button_material_square_deep_orange,
+                R.drawable.button_material_rounded_deep_orange, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(23, R.drawable.button_material_square_brown,
+                R.drawable.button_material_rounded_brown, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(24, R.drawable.button_material_square_grey,
+                R.drawable.button_material_rounded_grey, R.color.md_white_1000_transparent90));
+        possibleLayouts.add(new DrawableLayout(25, R.drawable.button_material_square_blue_grey,
+                R.drawable.button_material_rounded_blue_grey, R.color.md_white_1000_transparent90));
 
     }
 
